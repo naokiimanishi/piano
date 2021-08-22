fe3c05eb462cd4ec12b7594cfa27c3370d68a0eb
naokiimanishi <76042575+naokiimanishi@users.noreply.github.com>
2021-08-22 16:42:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const toggle = (() => {
-    const menu = document.getElementsByClassName("menu");
-
-    const nav = document.getElementsByClassName("header-navi");
+(function() {
+    const menu = document.getElementsByClassName("menuicon");
+    const nav = document.getElementsByClassName("header-nav");
+    const mask = document.getElementsByClassName("mask");
+    const span = document.getElementsByTagName("span");
 
     menu[0].addEventListener("click", () => {
-        nav[0].classList.add("sp");
-        console.log(nav[0]);
-    })
-})();
+        nav[0].classList.toggle("toggle");
+        mask[0].classList.toggle("mask-on");
+        span[0].classList.toggle("cross1");
+        span[1].classList.toggle("cross2");
+        span[2].classList.toggle("cross3");
+        menu[0].classList.toggle("box");
+    });
+    mask[0].addEventListener("click", () => {
+        nav[0].classList.toggle("toggle");
+        mask[0].classList.toggle("mask-on");
+        span[0].classList.toggle("cross1");
+        span[1].classList.toggle("cross2");
+        span[2].classList.toggle("cross3");
+        menu[0].classList.toggle("box");
+    });
+
+}());
